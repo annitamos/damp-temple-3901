3aec6351ca557dc678e0e75faf21bc49708d5bb2
amorou <annitamos@hotmail.com>
2013-03-08 16:55:48 +0200

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DampTemple3901::Application.config.secret_token = '5ecce12cf56dd43654ed55ad48740f8e21bcf3a16108fbca0a8403c41b4f888d8954406deefb3966a4a99c12e655f31845eae53030bd1c4219811f7def7c3184'
